e5f1a1f9b9cea7f0c8c7ad9434d11ea97eda652d
gamzedmr <77584427+gamzedmr@users.noreply.github.com>
2021-01-21 16:03:47 +0300

    Add project files.

---
+++ Kampintro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "kategori";
+            int ogrenciSayisi = 32000;
+            bool sistemeGırısYapmisMi = false;
+
+            if (sistemeGırısYapmisMi==true)
+            {
+                Console.WriteLine("kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("giriş yap butonu");
+   
+            }
+            
+
+
+            Console.WriteLine(kategoriEtiketi);
+           
+        }
+    }
+}
